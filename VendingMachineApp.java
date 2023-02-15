328a7fe1dd480a7b10ca12c786b3983ce7f8f784
Sean-McCue <sjmccue@quinnipiac.edu>
2023-02-15 13:48:38 -0500

    Added items to list

--- VendingMachineApp.java
+++ VendingMachineApp.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class VendingMachineApp 
 {
     Scanner userInput = new Scanner(System.in);
+    private static VendingMachine vendingMachine = new VendingMachine();
 
     //Function to fill the vending machine
-    public void fillMachine()
+    public static void fillMachine()
     {
-        //code
+        Item chips = new Item("chips", 2);
+        Item soda = new Item( "soda", 1);
+        Item water = new Item( "water", 1);
+
+        vendingMachine.addItem(water);
+        vendingMachine.addItem(soda);
+        vendingMachine.addItem(chips);
     }
     
     public static void main(String[] args) 
     {
-        
+        fillMachine();
     }
 }
+
